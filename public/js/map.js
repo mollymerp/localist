a34cd9c5da4dc551f7a8d5e78eb67f6996dff3fc
mollymerp <mollylloyd@gmail.com>
2015-10-19 17:55:56 -0700

    figured out mouse click coordinates

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,11 @@
 
 
 L.mapbox.accessToken = 'pk.eyJ1IjoibWxsb3lkIiwiYSI6Im9nMDN3aW8ifQ.mwiVAv4E-1OeaoR25QZAvw';
-L.mapbox.map('dc-map','mlloyd.noehc1pf', {
+var map = L.mapbox.map('dc-map','mlloyd.noehc1pf', {
   scrollWheelZoom: false,
   minZoom: 12
 }).setView([38.898, -77.046], 12);
 
+map.on('click', function(e) {
+    console.log(e.latlng);
+});
